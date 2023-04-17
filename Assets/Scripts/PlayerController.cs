d9a0cf09d838e0df8b4930bc772a45e98ba33bfa
BauerPhilipp <fam.bauer@live.at>
2023-04-17 16:05:46 +0200

    Combat

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,11 +10,15 @@ public class PlayerController : MonoBehaviour
     PlayerControls playerControls;
     Vector2 movement;
     Rigidbody2D rb;
+    Animator myAnimator;
+    SpriteRenderer mySpriteRenderer;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -28,18 +32,37 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+
     }
 
     private void Move()
     {
         rb.MovePosition(rb.position + movement * (Time.fixedDeltaTime * moveSpeed));
     }
+    void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        
+        if(mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else
+        {
+            mySpriteRenderer.flipX = false;
+        }
 
+    }
 
 }
